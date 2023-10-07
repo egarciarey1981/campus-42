69efd2e620cd6957438f5130f19322ff16299fb4
eliegarc <eliegarc@student.42malaga.com>
2023-10-07 20:06:14 +0200

    strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -21,7 +21,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	while (i < dstsize - 1 && src[i] != '\0')
 	{
-		dst[i] = (char)src[i];
+		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
