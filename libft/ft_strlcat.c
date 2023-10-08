8d5aa0d53e9e25452681235244190a8644afb61d
Eliecer García Rey <egarciarey1981@gmail.com>
2023-10-08 10:02:29 +0200

    strlcat

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eliegarc <eliegarc@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 10:01:13 by eliegarc          #+#    #+#             */
+/*   Updated: 2023/10/08 10:01:15 by eliegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (dst[i] != '\0')
+		i++;
+	if (i > dstsize)
+		return (ft_strlen(src) + dstsize);
+	while (src[j] != '\0' && i < dstsize - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	while (src[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+	return (i);
+}
