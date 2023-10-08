5bb1ca5274e74061cd3e091b11802693ea9adbf7
Eliecer García Rey <egarciarey1981@gmail.com>
2023-10-08 11:04:28 +0200

    revisión de includes

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
 #include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t len)
