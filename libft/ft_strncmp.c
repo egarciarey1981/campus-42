7277376f042aefd7190d170e5370a0a60eb579a7
Eliecer García Rey <egarciarey1981@gmail.com>
2023-10-08 10:58:00 +0200

    strncmp con punteros

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -16,14 +16,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t len)
 {
-	size_t	i;
-
-	i = 0;
-	while (s1[i] == s2[i] && i < len && s1[i] != '\0' && s2[i] != '\0')
-		i++;
-	if (i == len || (unsigned char)s1[i] == (unsigned char)s2[i])
+	while (*s1 == *s2 && len > 0 && *s1 != '\0' && *s2 != '\0')
+	{
+		s1++;
+		s2++;
+		len--;
+	}
+	if (len == 0 || (unsigned char)*s1 == (unsigned char)*s2)
 		return (0);
-	else if ((unsigned char)s1[i] > (unsigned char)s2[i])
+	else if ((unsigned char)*s1 > (unsigned char)*s2)
 		return (1);
 	else
 		return (-1);
