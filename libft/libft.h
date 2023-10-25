2e6489230d3e95af6ac906c28a31457727abdddb
Eliecer García Rey <egarciarey1981@gmail.com>
2023-10-25 20:03:02 +0200

    putchar_fd

--- libft/libft.h
+++ libft/libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 # include <stddef.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 int		ft_atoi(const char *nptr);
 void	ft_bzero(void *b, size_t len);
@@ -43,5 +44,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+void	ft_putchar_fd(char c, int fd);
 
 #endif
