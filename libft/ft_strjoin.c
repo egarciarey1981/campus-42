fb2e73f7fad3fcb34e7cd8dc7dbd61fd64cbcbbb
Eliecer García Rey <egarciarey1981@gmail.com>
2023-10-25 20:47:33 +0200

    norminette ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -20,7 +20,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-
 	str = ft_calloc(len1 + len2 + 1, sizeof(char));
 	ft_memcpy(str, s1, len1);
 	ft_memcpy(&str[len1], s2, len2);
