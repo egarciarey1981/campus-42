c89640e985e23567167b1aa50f018049d4a5d34e
Eliecer Garcia Rey <egarciarey1981@gmai.com>
2023-11-06 22:24:59 +0100

    fix strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -21,8 +21,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	str = ft_calloc(len1 + len2 + 1, sizeof(char));
-	ft_memcpy(str, s1, len1);
-	ft_memcpy(&str[len1], s2, len2);
-	str[len1 + len2 + 1] = '\0';
+	if (str == NULL)
+		return (NULL);
+	if (len1 > 0)
+		ft_strlcpy(str, s1, len1 + 1);
+	if (len2 > 0)
+		ft_strlcpy(str + len1, s2, len2 + 1);
 	return (str);
 }
