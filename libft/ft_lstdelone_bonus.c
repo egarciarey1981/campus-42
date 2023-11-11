78675e3c83b2b60e3f93267ce64bab410fa4f7a5
Eliecer García Rey <egarciarey1981@gmail.com>
2023-11-11 14:59:30 +0100

    lstdelone

---
+++ libft/ft_lstdelone_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eliegarc <eliegarc@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 14:49:03 by eliegarc          #+#    #+#             */
+/*   Updated: 2023/11/11 14:49:05 by eliegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst != NULL)
+	{
+		del(lst->content);
+		free(lst);
+	}
+}
