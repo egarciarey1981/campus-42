4960b7360fdc6866469a061616e1701d41d2cbe7
Eliecer García Rey <egarciarey1981@gmail.com>
2023-11-12 09:21:59 +0100

    lstiter

---
+++ libft/ft_lstiter_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eliegarc <eliegarc@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 09:20:43 by eliegarc          #+#    #+#             */
+/*   Updated: 2023/11/12 09:20:45 by eliegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst != NULL)
+	{
+		f(lst->content);
+		ft_lstiter(lst->next, f);
+	}
+}
